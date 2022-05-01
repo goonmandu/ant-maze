b720db718e90960ae8b7e7fd9d51c9824a6109da
goonmandu <hwang.mingi7@gmail.com>
2022-05-01 16:53:00 -0400

    Made steps less verbose

--- make.bat
+++ make.bat
@@ -1,9 +1,5 @@
-ECHO "Compiling ant.c"
 gcc -c ant.c
-ECHO "Compiling main.c"
 gcc -c main.c
-ECHO "Linking main.c and ant.c into main.exe"
 gcc -o main main.c main.c
-ECHO "Running main.exe"
 main.exe
 PAUSE
